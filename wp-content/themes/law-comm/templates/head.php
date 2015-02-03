4c1a658d47c2d0a8a9c5436acd26aeeb2904d904
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-02-03 13:45:21 +0000

    Changed main font from Fira to Roboto

--- wp-content/themes/law-comm/templates/head.php
+++ wp-content/themes/law-comm/templates/head.php
@@ -9,5 +9,5 @@
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
-  <link href='http://fonts.googleapis.com/css?family=Fira+Sans:400,500,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
 </head>
