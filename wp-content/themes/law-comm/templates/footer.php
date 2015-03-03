bccec01c0b3c43f0971c76a6261c7c4936640f24
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-03-03 09:41:02 +0000

    Added secondary nav links to footer

--- wp-content/themes/law-comm/templates/footer.php
+++ wp-content/themes/law-comm/templates/footer.php
@@ -1,9 +1,9 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
-    <div id="footer-links">
-    <a href="#">Privacy statement</a> | <a href="#">Justice on GOV.UK</a> | <a href="#">Sitemap</a>
-    </div>
+        <div id="footer-links">
+    <?php wp_nav_menu( array('menu' => 'Footer links' )); ?>
+    </div> 
   </div>
 </footer>
 
