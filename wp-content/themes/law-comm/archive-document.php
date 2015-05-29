454671861e1e3d99656f68fc71dd7550e27a5fe5
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-05-29 11:58:05 +0100

    Amended document search page to display published date

--- wp-content/themes/law-comm/archive-document.php
+++ wp-content/themes/law-comm/archive-document.php
@@ -15,7 +15,14 @@
 <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
   <?php $project = get_field('project');  ?>
   <?php if($project): ?>
-    <a class="doc-list" href="/project/<?= $project->post_name; ?>/#<?= $post->post_name; ?>">Project: <?= $project->title; ?> | Publication: <?= $post->title; ?></a>
+    <h3><a class="doc-list" href="/project/<?= $project->post_name; ?>/#<?= $post->post_name; ?>">Project: <?= $project->title; ?> | Publication: <?= $post->title; ?></a></h3>
+    <?php $date = DateTime::createFromFormat('Y-m-d', get_field('publication_date')); ?>
+    <?php if (!empty($date)): ?>
+
+      <p><strong>Publication date:</strong> <?php echo $date->format('j F Y') ?></p>
+
+      <?php endif; ?>
+      <hr>
   <?php endif; ?>
 <?php endwhile; ?>
 <?php endif; ?>
