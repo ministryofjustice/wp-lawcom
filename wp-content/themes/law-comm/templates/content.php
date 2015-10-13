7d2a2b4d2525fc4a5070fbd46a2876aff23ca0fd
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2015-10-13 13:56:55 +0100

    Remove publication date from project summary listing.

--- wp-content/themes/law-comm/templates/content.php
+++ wp-content/themes/law-comm/templates/content.php
@@ -1,7 +1,9 @@
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <p><?php get_template_part('templates/entry-meta'); ?></p>
+    <?php if (get_post_type() !== 'project'): ?>
+      <p><?php get_template_part('templates/entry-meta'); ?></p>
+    <?php endif; ?>
   </header>
   <div class="entry-summary">
     <?php the_excerpt(); ?>
