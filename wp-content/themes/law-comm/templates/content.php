05c856d98cfed0fd992a1aa3e223ba4d1d1bee16
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2015-10-13 15:48:00 +0100

    Adjust the display of search results.
    – Prepend result title with 'Project' for project results, to allow users to differentiate between projects and other results.
    – Exclude 'document' post type from search results. This information can & should be viewed from the main project page which the document is associated with.

--- wp-content/themes/law-comm/templates/content.php
+++ wp-content/themes/law-comm/templates/content.php
@@ -1,6 +1,18 @@
 <article <?php post_class(); ?>>
   <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2 class="entry-title">
+      <a href="<?php the_permalink(); ?>">
+        <?php
+
+        if (is_search() && get_post_type() == 'project') {
+          echo 'Project: ';
+        }
+
+        the_title();
+
+        ?>
+      </a>
+    </h2>
     <?php if (get_post_type() !== 'project'): ?>
       <p><?php get_template_part('templates/entry-meta'); ?></p>
     <?php endif; ?>
