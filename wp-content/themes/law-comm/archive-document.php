ce4a2968880f590c8ec4cae10273b6e98e2779a2
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-09-15 12:16:52 +0100

    Show 'no results' message when no results were found in the publication search.

--- wp-content/themes/law-comm/archive-document.php
+++ wp-content/themes/law-comm/archive-document.php
@@ -1,38 +1,36 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'roots'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
-
 <?php
   $wp_query = document_query();
   //p2p_type( 'projects_to_documents' )->each_connected( $wp_query );
 ?>
 <?php if ( $wp_query->have_posts() ) : ?>
-<?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-  <?php $project = get_field('project');  ?>
-  <?php
-    if(!empty($project->post_name)) {
-      $url = "/project/" . $project->post_name . "/#" . $post->post_name;;
-    } else {
-      $url = get_permalink( );
-    }
-  ?>
-  <?php //if($project): ?>
-    <h3><a class="doc-list" href="<?= $url; ?>">Publication: <?= $post->title; ?></a></h3>
-    <?php $date = DateTime::createFromFormat('Y-m-d', get_field('publication_date')); ?>
-    <?php if (!empty($date)): ?>
+  <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+    <?php $project = get_field('project');  ?>
+    <?php
+      if(!empty($project->post_name)) {
+        $url = "/project/" . $project->post_name . "/#" . $post->post_name;;
+      } else {
+        $url = get_permalink( );
+      }
+    ?>
+    <?php //if($project): ?>
+      <h3><a class="doc-list" href="<?= $url; ?>">Publication: <?= $post->title; ?></a></h3>
+      <?php $date = DateTime::createFromFormat('Y-m-d', get_field('publication_date')); ?>
+      <?php if (!empty($date)): ?>
 
-      <p><strong>Publication date:</strong> <?php echo $date->format('j F Y') ?></p>
-      <p><strong>Response date:</strong> <?php the_field('response_date'); ?></p>
+        <p><strong>Publication date:</strong> <?php echo $date->format('j F Y') ?></p>
+        <p><strong>Response date:</strong> <?php the_field('response_date'); ?></p>
 
-      <?php endif; ?>
-      <hr>
-  <?php //endif; ?>
-<?php endwhile; ?>
+        <?php endif; ?>
+        <hr>
+    <?php //endif; ?>
+  <?php endwhile; ?>
+<?php else: ?>
+  <div class="alert alert-warning">
+    <?php _e('Sorry, no results were found.', 'roots'); ?>
+  </div>
+  <p>Please adjust your search and try again.</p>
 <?php endif; ?>
 
 <div class="pagination">
