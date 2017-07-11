24b61e51803bbde8e681387402809eb7aa268d9b
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-07-11 17:05:15 +0100

    Remove Google Analytics tracking from theme.

--- web/app/themes/lawcom/lib/scripts.php
+++ web/app/themes/lawcom/lib/scripts.php
@@ -9,10 +9,6 @@
  * 1. jquery-1.11.1.min.js via Google CDN
  * 2. /theme/assets/js/vendor/modernizr.min.js
  * 3. /theme/assets/js/scripts.js (in footer)
- *
- * Google Analytics is loaded after enqueued scripts if:
- * - An ID has been defined in config.php
- * - You're not logged in as an administrator
  */
 function roots_scripts() {
   /**
@@ -76,29 +72,3 @@ function roots_jquery_local_fallback($src, $handle = null) {
   return $src;
 }
 add_action('wp_head', 'roots_jquery_local_fallback');
-
-/**
- * Google Analytics snippet from HTML5 Boilerplate
- *
- * Cookie domain is 'auto' configured. See: http://goo.gl/VUCHKM
- */
-function roots_google_analytics() { ?>
-<script>
-  <?php if (WP_ENV === 'production') : ?>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='//www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-  <?php else : ?>
-    function ga() {
-      console.log('GoogleAnalytics: ' + [].slice.call(arguments));
-    }
-  <?php endif; ?>
-  ga('create','<?php echo GOOGLE_ANALYTICS_ID; ?>','auto');ga('send','pageview');
-</script>
-
-<?php }
-if (GOOGLE_ANALYTICS_ID && (WP_ENV !== 'production' || !current_user_can('manage_options'))) {
-  add_action('wp_footer', 'roots_google_analytics', 20);
-}
