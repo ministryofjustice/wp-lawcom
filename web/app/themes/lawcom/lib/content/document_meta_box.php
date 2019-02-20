44eb5290a4ae6c0e1ce9ed25bb1331a8818ca43c
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-02-20 15:44:25 +0000

    Bug fix: ACF not displaying content in admin. Removed use of conflicting WP_Query

--- web/app/themes/lawcom/lib/content/document_meta_box.php
+++ web/app/themes/lawcom/lib/content/document_meta_box.php
@@ -1,39 +1,42 @@
 <?php
 
-function newDocumentMetaBox() {
+function newDocumentMetaBox()
+{
   return new documentMetaBox();
 }
 
-if ( is_admin() && isset( $_GET['post'] ) ) {
-    add_action( 'load-post.php', 'newDocumentMetaBox' );
-    add_action( 'load-post-new.php', 'newDocumentMetaBox' );
+if (is_admin() && isset($_GET['post'])) {
+  add_action('load-post.php', 'newDocumentMetaBox');
+  add_action('load-post-new.php', 'newDocumentMetaBox');
 }
 
 /**
  * The Class.
  */
-class documentMetaBox {
+class documentMetaBox
+{
 
   /**
    * Hook into the appropriate actions when the class is constructed.
    */
-  public function __construct() {
-    add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
-    add_action( 'save_post', array( $this, 'save' ) );
+  public function __construct()
+  {
+    add_action('add_meta_boxes', array($this, 'add_meta_box'));
+    add_action('save_post', array($this, 'save'));
   }
 
   /**
    * Adds the meta box container.
    */
-  public function add_meta_box( $post_type )
+  public function add_meta_box($post_type)
   {
     $post_types = array('document', 'project');
 
-    if ( in_array( $post_type, $post_types )) {
+    if (in_array($post_type, $post_types)) {
       add_meta_box(
         'project_finder',
-        __( 'Project / Document Finder', 'roots' ),
-        array( $this, 'render_meta_box_content' ),
+        __('Project / Document Finder', 'roots'),
+        array($this, 'render_meta_box_content'),
         $post_type,
         'side',
         'high'
@@ -46,7 +49,7 @@ class documentMetaBox {
    *
    * @param int $post_id The ID of the post being saved.
    */
-  public function save( $post_id )
+  public function save($post_id)
   {
     // This only outputs content
   }
@@ -57,17 +60,16 @@ class documentMetaBox {
    *
    * @param WP_Post $post The post object.
    */
-  public function render_meta_box_content( $post )
+  public function render_meta_box_content($post)
   {
     $post_type = get_post_type();
-    if( $post_type == 'document' ) {
+    $query_id = $post->ID;
+    if ($post_type == 'document') {
       $project = get_field('field_54d37be93594e', $post->ID);
-      if( !empty($project) ) {
-        echo '<p><strong>Project:</strong> <a href="' . get_edit_post_link($project->ID) . '">' .  get_the_title($project->ID) . '</a></p>';
+      if (!empty($project)) {
+        echo '<p><strong>Project:</strong> <a href="' . get_edit_post_link($project->ID) . '">' . get_the_title($project->ID) . '</a></p>';
+        $other_id = $query_id = $project->ID;
       }
-      $id = $project->ID;
-    } else {
-      $id = $post->ID;
     }
 
     $args = array(
@@ -75,27 +77,26 @@ class documentMetaBox {
       'posts_per_page' => -1,
       'meta_query' => array(
         array(
-          'key'     => 'project',
-          'value'   => $id,
+          'key' => 'project',
+          'value' => $query_id,
         ),
       ),
     );
-    $query = new WP_Query($args);
-    if ( $query->have_posts() ) {
-      if( !empty($project->ID) ) {
+
+
+    $documents = get_posts($args);
+
+    if (!empty($documents)) {
+      if (!empty($other_id)) {
         echo '<strong>Other documents for this project:</strong><ul>';
       } else {
         echo '<strong>Documents for this project:</strong><ul>';
       }
 
-      while ( $query->have_posts() ) {
-        $query->the_post();
-        echo '<li><a href="' . get_edit_post_link() . '">' . get_the_title() . '</a></li>';
+      foreach ($documents as $document) {
+        echo '<li><a href="' . get_edit_post_link($document->ID) . '">' . get_the_title($document->ID) . '</a></li>';
       }
       echo '</ul>';
     }
-    wp_reset_postdata();
-
   }
-
 }
