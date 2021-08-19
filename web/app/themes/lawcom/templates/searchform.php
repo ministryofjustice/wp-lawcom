183c369ced41038471fb8e15ac2e790cd03b9d55
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2021-08-19 13:08:34 +0100

    alt text for image

--- web/app/themes/lawcom/templates/searchform.php
+++ web/app/themes/lawcom/templates/searchform.php
@@ -3,7 +3,7 @@
   <div class="input-group">
     <input id="searchbox" type="search" value="<?php echo get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search the site', 'roots'); ?>">
     <span class="input-group-btn">
-      <button type="submit" class="search-submit btn btn-default"><?php _e('', 'roots'); ?><img src="<?php bloginfo('template_directory'); ?>/assets/img/green-search-icon.gif"></button>
+      <button type="submit" class="search-submit btn btn-default"><?php _e('', 'roots'); ?><img alt="search button" src="<?php bloginfo('template_directory'); ?>/assets/img/green-search-icon.gif"></button>
     </span>
   </div>
 </form>
