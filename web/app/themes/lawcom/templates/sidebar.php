5e7135681e6726f3188620294487767982e2c746
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2022-03-02 10:57:55 +0000

    Fix Menu

--- web/app/themes/lawcom/templates/sidebar.php
+++ web/app/themes/lawcom/templates/sidebar.php
@@ -1,8 +1,17 @@
-<?php 
+<div class="quick-links side-item">
+  <ul>
+    <li class="ql1"><a href="<?= home_url('/project/'); ?>">Find a Project</a></li>
+    <li class="ql2"><a href="<?= home_url('/document/?document-type=consultations-related-documents&consultation-status=open'); ?>">Find Open Consultations</a></li>
+    <li class="ql3"><a href="<?= home_url('/document/'); ?>">Find a Publication</a></li>
+  </ul>
+</div>
+<?php
 	if (is_front_page()){
 		dynamic_sidebar('sidebar-homepage');
 		}
 	else {
 	dynamic_sidebar('sidebar-primary'); 
 	}
+
 ?>
+
